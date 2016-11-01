cf6dc3ff725c4d4ae2e6c7aee4ca9c4eb3cef2c3
Oleksandr Chornobai <Oleksandr_Chornobai@epam.com>
2016-11-01 18:05:16 +0300

    timer

---
+++ productive-app/js/tempaltes.js
@@ -0,0 +1,5 @@
+
+console.log(document.getElementsByClassName('button-holder')[0].innerHTML);
+document.getElementsByClassName('button-holder')[0].innerHTML = document.getElementById('settings-template-button-next-green').innerHTML;
+document.getElementsByClassName('button-holder')[0].appendChild(document.getElementById('settings-template-button-next-green').content.cloneNode(true));
+
