1a509f91a35eec0100292c70479b6b74a494b415
Oleksandr Chornobai <Oleksandr_Chornobai@epam.com>
2016-12-28 21:55:28 +0200

    sdf

--- productive-app/services/notifications.js
+++ productive-app/services/notifications.js
@@ -1,27 +1,29 @@
-var Notifications = {
-    spawnNotification(opt) {
-        if (!("Notification" in window)) {
-            alert("This browser does not support system notifications");
+(function () {
+    window.Notifications = {
+        spawnNotification(opt) {
+            if (!("Notification" in window)) {
+                alert("This browser does not support system notifications");
+            }
+            else if (Notification.permission === "granted") {
+                var options = {
+                    body: opt.body,
+                    icon: opt.icon
+                };
+                var n = new Notification(opt.title, options);
+                setTimeout(n.close.bind(n), 5000);
+            }
+            else if (Notification.permission !== 'denied') {
+                Notification.requestPermission(function (permission) {
+                    if (permission === "granted") {
+                        var options = {
+                            body: opt.body,
+                            icon: opt.icon
+                        };
+                        var n = new Notification(opt.title, options);
+                        setTimeout(n.close.bind(n), 5000);
+                    }
+                });
+            }
         }
-        else if (Notification.permission === "granted") {
-            var options = {
-                body: opt.body,
-                icon: opt.icon
-            };
-            var n = new Notification(opt.title, options);
-            setTimeout(n.close.bind(n), 5000);
-        }
-        else if (Notification.permission !== 'denied') {
-            Notification.requestPermission(function (permission) {
-                if (permission === "granted") {
-                    var options = {
-                        body: opt.body,
-                        icon: opt.icon
-                    };
-                    var n = new Notification(opt.title, options);
-                    setTimeout(n.close.bind(n), 5000);
-                }
-            });
-        }
-    }
-};
+    };
+}());
